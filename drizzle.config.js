1f445cf7a835a5f732cb9739fd5bbb3a78201c86
naveedrashid <naveed.pixelz360@gmail.com>
2025-09-09 10:37:55 +0500

    cms file/folder structure completed with db create

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "mysql",
-  schema: "./src/lib/db/schema/*",
+  dialect: "postgresql",
+  schema: "./src/db/**/*.js",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL,
--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
+import { Pool } from "pg";
 import { drizzle } from 'drizzle-orm/node-postgres';
-import pkg from 'pg';
-const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export const db = drizzle(pool);
